0798e409aaabf716b75cf6d02d5d3035b691881b
kostyak <kostyak7676@gmail.com>
2015-07-08 07:51:05 +0300

    trainerOne: now goes through all dictionary

--- background/trainerOne.js
+++ background/trainerOne.js
@@ -19,12 +19,12 @@
 
     var writePhrase = function (rawPhrase) {
         /*rawPhrase example:
-        * ["j0YwJe6T61w", "ru", "en", "произвольный", "arbitrary", 1413313880579293]
-        * */
+         * ["j0YwJe6T61w", "ru", "en", "произвольный", "arbitrary", 1413313880579293]
+         * */
         var native = null;
         var to = null;
         var toDictKey = null;
-        if (rawPhrase[1] == nativeDictKey){
+        if (rawPhrase[1] == nativeDictKey) {
             native = rawPhrase[3];
             to = rawPhrase[4];
             toDictKey = rawPhrase[2];
@@ -56,30 +56,22 @@
      * @param length of an array
      * @returns {number}
      */
-    var getRandomIndex = function(length){
-       return Math.floor(Math.random() * length);
-    };
-
-    var getRandomIndexes = function (count) {
-        var _result = [];
-        for (var i= 0; i < choicesLength; i++){
-            _result.push(getRandomIndex(dictionary.length));
-        }
-        return _result;
+    var getRandomIndex = function (length) {
+        return Math.floor(Math.random() * length);
     };
 
     /**
      * gets first Element in array and inserts it randomly in arr
      * @param arr
      */
-    var shakeFirstElem = function(arr){
+    var shakeFirstElem = function (arr) {
         var len = arr.length;
         var newIndex = getRandomIndex(len);
-        if (newIndex == 0){
+        if (newIndex == 0) {
             return;
         }
         var first = arr.shift();
-        if (newIndex == len){
+        if (newIndex == len) {
             arr.append();
         } else {
             arr.splice(newIndex, 0, first);
@@ -87,11 +79,70 @@
     };
 
     /**
+     * @param arr Array to be shake
+     * @returns undefined
+     */
+    var shakeArray = function (arr) {
+        /*todo should define more complicate algorithm*/
+        for (var i = 0, l = arr.length; i < l; i++) {
+            shakeFirstElem(arr);
+        }
+    };
+    /**
+     * holds shaken phrases and returns them one by one as long as it is necessary
+     *
+     * use:
+     *  randomPhrases.get();
+     *  todo build more getters for different phrase types
+     */
+    var randomBasePhrases = (function () {
+        var _indexes = [];
+        var _getInitialIndexes = function () {
+            var _inArr = [];
+            utils.forEach(dictionary, function (elem, i) {
+                _inArr.push(i);
+            });
+            return _inArr;
+        };
+        var _getIndex = function () {
+            if (_indexes.length == 0) {
+                _indexes = _getInitialIndexes();
+                shakeArray(_indexes);
+            }
+            return _indexes.shift();
+        };
+        var get = function () {
+            return dictionary[_getIndex()];
+        };
+        return {get: get};
+    })();
+
+    /**
      * returns phrase to learn
      */
-    var getPhrase = function(){
+    var getPhrase = function () {
         /*todo*/
         return dictionary[getRandomIndex(dictionary.length)];
+    };
+
+    /**
+     * returns array of phrases where first element is the phrase to train
+     * @param len: is needed length of array to be returned
+     */
+    var getPhrases = function (len) {
+        var _ids = {};
+        var _phrase = getPhrase();
+        _ids[_phrase.id] = true;
+        var _phrases = [_phrase];
+
+        while (_phrases.length < len) {
+            var _possiblePhrase = randomBasePhrases.get();
+            if (!(_possiblePhrase.id in _ids)) {
+                _ids[_possiblePhrase.id] = true;
+                _phrases.push(_possiblePhrase);
+            }
+        }
+        return _phrases;
     };
 
     /**
@@ -114,12 +165,12 @@
             choices: []
         };
         var isFirst = true;
-        utils.forEach(getRandomIndexes(choicesLength), function(i){
-            if (isFirst){
-                test.phrase = dictionary[i].from;
+        utils.forEach(getPhrases(choicesLength), function (phrase) {
+            if (isFirst) {
+                test.phrase = phrase.from;
             }
             test.choices.push({
-                choice: dictionary[i].to,
+                choice: phrase.to,
                 isCorrect: isFirst
             });
             isFirst = false;
